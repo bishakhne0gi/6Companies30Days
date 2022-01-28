1519eaaa888f06d57b8db462d499f4d12d9ed3e6
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2022-01-28 20:58:20 +0530

    Microsoft

--- Microsoft/BinaryNumbers.cpp
+++ Microsoft/BinaryNumbers.cpp
@@ -1,31 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 vector<string> generate(int N)
 {
-	// Your code here
-	vector<string>v;
-	queue<string>q;
-	q.push("1");
-	while(N--){
-	    string temp=q.front();
-	    v.push_back(temp);
-	    q.pop();
-	    q.push(temp+'0');
-	    q.push(temp+'1');
-	}
-	return v;
+    // Your code here
+    vector<string> v;
+    queue<string> q;
+    q.push("1");
+    while (N--)
+    {
+        string temp = q.front();
+        v.push_back(temp);
+        q.pop();
+        q.push(temp + '0');
+        q.push(temp + '1');
+    }
+    return v;
 }
 int main()
 {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cin>>n;
-		vector<string> ans = generate(n);
-		for(auto it:ans) cout<<it<<" ";
-		cout<<endl;
-	}
-	return 0;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<string> ans = generate(n);
+        for (auto it : ans)
+            cout << it << " ";
+        cout << endl;
+    }
+    return 0;
 }
