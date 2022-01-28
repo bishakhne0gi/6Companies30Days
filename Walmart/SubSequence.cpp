a6bbac49e274ded66377a30c71004946c6cfa107
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2022-01-28 12:33:27 +0530

    Walmart

---
+++ Walmart/SubSequence.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isSubSequence(vector<int> &v1, vector<int> &v2, int n, int m)
+{
+
+    if (m == 0)
+        return true;
+    if (n == 0)
+        return false;
+
+    if (v1[n - 1] == v2[m - 1])
+        return isSubSequence(v1, v2, n - 1, m - 1);
+
+    return isSubSequence(v1, v2, n - 1, m);
+}
+class Solution
+{
+public:
+    vector<int> find3Numbers(vector<int> arr, int N)
+    {
+        int prv = arr[0];
+        vector<int> v;
+        int mx = -1;
+        for (int i = N - 1; i >= 0; i--)
+        {
+            mx = max(mx, arr[i]);
+            v.push_back(mx);
+        }
+
+        reverse(v.begin(), v.end());
+        for (int i = 1; i < N - 1; i++)
+        {
+            if (arr[i] > prv && arr[i] < v[i + 1])
+                return {prv, arr[i], v[i + 1]};
+            prv = min(prv, arr[i]);
+        }
+
+        return {};
+    }
+};
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        Solution obj;
+        auto res = obj.find3Numbers(a, n);
+        if (!res.empty() and res.size() != 3)
+        {
+            cout << -1 << "\n";
+        }
+
+        if (res.empty())
+        {
+            cout << 0 << "\n";
+        }
+        else if ((res[0] < res[1] and res[1] < res[2]) and
+                 isSubSequence(a, res, n, res.size()))
+        {
+            cout << 1 << "\n";
+        }
+        else
+        {
+            cout << -1 << "\n";
+        }
+    }
+
+    return 0;
+}
