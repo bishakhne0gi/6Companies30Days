361d23fc45e88f129e4305baa0e2bdd9c2f05c2c
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2022-01-28 10:32:00 +0530

    Microsoft

---
+++ Microsoft/TraverseSpirally.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> spirallyTraverse(vector<vector<int>> matrix, int r, int c)
+    {
+
+        vector<int> ans;
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int count = 0;
+        int total = row * col;
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = row - 1;
+        int endingCol = col - 1;
+
+        while (count < total)
+        {
+            for (int index = startingCol; count < total && index <= endingCol; index++)
+            {
+                ans.push_back(matrix[startingRow][index]);
+                count++;
+            }
+            startingRow++;
+            for (int index = startingRow; count < total && index <= endingRow; index++)
+            {
+                ans.push_back(matrix[index][endingCol]);
+                count++;
+            }
+            endingCol--;
+            for (int index = endingCol; count < total && index >= startingCol; index--)
+            {
+                ans.push_back(matrix[endingRow][index]);
+                count++;
+            }
+            endingRow--;
+            for (int index = endingRow; count < total && index >= startingRow; index--)
+            {
+                ans.push_back(matrix[index][startingCol]);
+                count++;
+            }
+            startingCol++;
+        }
+        return ans;
+    }
+};
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r);
+
+        for (int i = 0; i < r; i++)
+        {
+            matrix[i].assign(c, 0);
+            for (int j = 0; j < c; j++)
+            {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix, r, c);
+        for (int i = 0; i < result.size(); ++i)
+            cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
